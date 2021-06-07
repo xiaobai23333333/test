1b447b8f65d9fd7cd99fbacb8b0d7427fbb2b991
wwb <1464230907@qq.com>
2021-06-07 21:31:03 +0800

    first commit

---
+++ 17.01.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: wwb
+ * @LastEditors: wwb
+ * @email: AbeRanDy@163.com
+ * @github: https://github.com/xiaobai23333333
+ * @Date: 2021-06-07 17:34:57
+ * @LastEditTime: 2021-06-07 21:31:02
+ * @Description: Modify here please
+ * @FilePath: \Chapter17\17.01.cpp
+ */
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <string>
+using namespace std;
+
+
+ int main()
+ {
+    auto t = tuple<int, int, int>{10, 20, 30};
+    // auto t1 = tuple<string, vector<string>, pair<string, int> >;
+    cin.get();
+
+ }
