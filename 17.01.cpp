f71af8003b66a56687219f88de7cc6bab771a4f6
wwb <1464230907@qq.com>
2021-06-08 10:44:01 +0800

    17.01

--- 17.01.cpp
+++ 17.01.cpp
@@ -4,7 +4,7 @@
  * @email: AbeRanDy@163.com
  * @github: https://github.com/xiaobai23333333
  * @Date: 2021-06-07 17:34:57
- * @LastEditTime: 2021-06-07 21:31:02
+ * @LastEditTime: 2021-06-08 10:42:43
  * @Description: Modify here please
  * @FilePath: \Chapter17\17.01.cpp
  */
@@ -19,7 +19,7 @@ using namespace std;
  int main()
  {
     auto t = tuple<int, int, int>{10, 20, 30};
-    // auto t1 = tuple<string, vector<string>, pair<string, int> >;
+    tuple<string, vector<string>, pair<string, int> > t1;
     cin.get();
 
  }
